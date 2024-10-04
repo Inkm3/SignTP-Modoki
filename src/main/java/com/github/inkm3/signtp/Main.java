35fa88407de2b067cef527a66f61555368c80f12
inkm3 <inakami1214.ikachi@gmail.com>
2024-10-04 18:28:50 +0900

    second commit

--- src/main/java/com/github/inkm3/signtp/Main.java
+++ src/main/java/com/github/inkm3/signtp/Main.java
@@ -1,17 +1,54 @@
 package com.github.inkm3.signtp;
 
+import com.github.inkm3.signtp.Commands.SignTPCommand;
+import com.github.inkm3.signtp.CustomEvents.SignTPEnableEvent;
+import com.github.inkm3.signtp.Events.ServerEvents;
+import com.github.inkm3.signtp.Events.SignEvents;
+import com.github.inkm3.signtp.FileEdit.ServerData;
+import com.github.inkm3.signtp.FileEdit.SignData;
+import com.github.inkm3.signtp.System.SignTP;
+import com.github.inkm3.signtp.Util.Config;
+import com.github.inkm3.signtp.Util.Message;
+import com.github.inkm3.signtp.Util.SignText;
+import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.CommandAPIBukkitConfig;
+import dev.jorel.commandapi.CommandAPICommand;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     @Override
-    public void onEnable() {
-        // Plugin startup logic
+    public void onLoad() {
+        CommandAPI.onLoad(new CommandAPIBukkitConfig(this).silentLogs(true));
+    }
 
+    @Override
+    public void onEnable() {
+        CommandAPI.onEnable();
+
+        Config.load();
+        ServerData.load();
+        SignData.load();
+        Message.load();
+        SignText.load();
+
+        ServerEvents.load();
+        SignEvents.load();
+        SignTPCommand.load();
+
+        getServer().getPluginManager().callEvent(new SignTPEnableEvent());
+
+        SignTP.startDataSyncLoop();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        CommandAPI.onDisable();
+
+        SignData.save();
+        ServerData.save();
     }
+
 }
